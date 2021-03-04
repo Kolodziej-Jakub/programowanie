2254d963840ce3a9996d5d2b0af08dced054de14
Kolodziej-Jakub <jakub.kolodziej@microsoft.wsei.edu.pl>
2021-03-04 14:20:44 +0100

    Dodanie drugiej lini

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Witaj świecieeeeeee!");
+            Console.WriteLine("Witaj świeciee!");
+            Console.WriteLine("Cześć");
         }
     }
 }
