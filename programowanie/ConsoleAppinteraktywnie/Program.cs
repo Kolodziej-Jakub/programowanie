6d864b0df6bef086a0211df2180065314f93ea64
Kolodziej-Jakub <jakub.kolodziej@microsoft.wsei.edu.pl>
2021-03-11 11:05:46 +0100

    Obsługa powitania

---
+++ programowanie/ConsoleAppinteraktywnie/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleAppinteraktywnie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Witaj!");
+
+
+            Console.Write("Podaj imię:");
+            string imie;
+            imie = Console.ReadLine();
+
+            Console.Write("Podaj Nazwisko:");
+            string Nazwisko;
+            Nazwisko = Console.ReadLine();
+
+            Console.WriteLine("Witaj" + imie + " " + Nazwisko + "!");
+
+           string napis = string.Format("Witaj {0} {1}!", imie, Nazwisko);
+            Console.WriteLine(napis);
+
+            Console.WriteLine("Witaj {0} {1}!", Nazwisko, imie);
+
+            Console.WriteLine( $"Witaj {imie} {Nazwisko}!" );
+            
+        }
+    }
+}
